ee3aa828fe934b78123b7d776be70c711f36f411
hoshikawa megumi <ddt.mehoshikawa@gmail.com>
2019-12-01 18:07:39 +0900

    　課題番号　E01_02 　100％

---
+++ maikai_java/src/e_01_01/E_01_02.java
@@ -0,0 +1,13 @@
+package e_01_01;
+/*演習1ｰ2
+右に指すように、各行に1文字ずつ名前を表示するプログラムを作成せよ
+著者の名前ではなく、自分の名前を表示すること
+*/
+
+public class E_01_02 {
+	public static void main(String[] args) {
+
+		// 名前を各行に1文字づつ出力させる為の命令文
+		System.out.println("星\n川\n恵");
+	}
+}
