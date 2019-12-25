9cf9e79d6f8144c87c974bf3b0089681e249e05f
hoshikawa megumi <ddt.mehoshikawa@gmail.com>
2019-12-25 20:40:43 +0900

    　E02_02 　三つのint型変数に値を代入し、合計と平均を求めるプログラムを作成する。 　100％

--- maikai_java/src/e_02_02/E_02_02.java
+++ maikai_java/src/e_02_02/E_02_02.java
@@ -6,21 +6,31 @@ package e_02_02;
 public class E_02_02 {
 	public static void main(String[] args) {
 
-		// 変数
-		int x;
-		int y;
-		int z;
+		// Aさんのテストの点数の合計・平均を求めるとする
+		// 変数（国語の点数）
+		int nationallanguageTest;
 
-		// 代入する値;
-		x = 12;
-		y = 20;
-		z = 40;
+		// 変数（数学の点数）
+		int mathTest;
+
+		// 変数（理科の点数）
+		int scienceTest;
+
+		int totalResult;
+
+		// 代入する点数;
+		nationallanguageTest = 63;
+		mathTest = 78;
+		scienceTest = 90;
+
+		// 合計結果
+		totalResult = (nationallanguageTest + mathTest + scienceTest);
 
 		// 出力（合計）
-		System.out.println(x + y + z);
+		System.out.println(totalResult);
 
 		// 出力（平均）
-		System.out.println((x + y + z) / 2);
+		System.out.println(totalResult / 3);
 
 	}
 }
