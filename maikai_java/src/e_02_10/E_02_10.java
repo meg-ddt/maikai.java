7e5cf34624740b7546902a768d0f4ee584ffc643
hoshikawa megumi <ddt.mehoshikawa@gmail.com>
2020-02-10 21:03:44 +0900

    　E02_10 　名前の性と名を個別にキーボードから読み込んで挨拶を行うプログラムの作成。 　100％

---
+++ maikai_java/src/e_02_10/E_02_10.java
@@ -0,0 +1,30 @@
+package e_02_10;
+
+/*演習2-10
+ 名前の性と名を個別にキーボードから読み込んで
+ 挨拶を行うプログラムを作成せよ。
+*/
+
+import java.util.Scanner;
+
+public class E_02_10 {
+
+	public static void main(String[] args) {
+
+		// キーボードとプログラムをつなぐための命令文
+		Scanner nameLast= new Scanner(System.in);
+		Scanner nameFirst = new Scanner(System.in);
+
+		// 読み込まれた値を抽出。
+		System.out.print("性 ： ");
+		// 抽出された値を代入。
+		String familyName = nameLast.next();
+
+		// 読み込まれた値を抽出。
+		System.out.print("名 ： ");
+		// 抽出された値を代入。
+		String firstName = nameFirst.next();
+
+		System.out.println("こんにちは" + familyName + firstName + "さん。");
+	}
+}
