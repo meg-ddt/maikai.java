b2d58d999789dbbc29d5e184c2f9e7530da06ed8
hoshikawa megumi <ddt.mehoshikawa@gmail.com>
2020-02-10 21:00:53 +0900

    　E02_09 　ご指摘柊生(変数名が内容とマッチしていない) 　100％

--- maikai_java/src/e_02_09/E_02_09.java
+++ maikai_java/src/e_02_09/E_02_09.java
@@ -21,18 +21,18 @@ public class E_02_09 {
 		Random rand = new Random();
 
 		// 0.0以上1.0未満の実数値をランダムに生成して表示
-		double highestTemperature = rand.nextDouble();
+		double timeDifferenceWithFirstPlace = rand.nextDouble();
 		// 0.0以上10.0未満の実数値をランダムに生成して表示
-		double lowestTemperature = rand.nextDouble() * 9;
+		double meterTime = rand.nextDouble() * 9;
 		// -1.0以上1.0未満の実数値をランダムに生成して表示
-		double sinceYears = rand.nextDouble() * -1;
+		double personalBest = rand.nextDouble() * -1;
 
 		// 生成された値を出力
-		System.out.println("1位との差は" + highestTemperature + "秒です。");
+		System.out.println("1位との差は" + timeDifferenceWithFirstPlace + "秒です。");
 		// 生成された値を出力
-		System.out.println("50mのタイムは" + lowestTemperature + "秒です。");
+		System.out.println("50mのタイムは" + meterTime + "秒です。");
 		// 生成された値を出力
-		System.out.println("自己ベストタイムとの差は" + sinceYears + "秒です。");
+		System.out.println("自己ベストとの差は" + personalBest + "秒です。");
 
 	}
 }
