00c6cda5ed28f30fc79ac3b666f7c0539c99a03a
hoshikawa megumi <ddt.mehoshikawa@gmail.com>
2020-02-12 20:18:05 +0900

    　E03_01 　整数値を読み込み、 絶対値を求めて表示するプログラムの作成。 　100％

---
+++ maikai_java/src/e_03_01/E_03_01.java
@@ -0,0 +1,36 @@
+package e_03_01;
+
+/*演習3-1
+ 整数値を読み込んで
+ その絶対値を求めて表示する
+ プログラムを作成せよ
+*/
+
+import java.util.Scanner;
+
+public class E_03_01 {
+
+	public static void main(String[] args) {
+
+		Scanner stdIn = new Scanner(System.in);
+
+		// 読み込まれた値を抽出。
+		System.out.print("整数値 ： ");
+		// 抽出された値を代入。
+		int integerValue = stdIn.nextInt();
+
+		// 整数値が0より大きい場合
+		if (integerValue > 0)
+			// 絶対値の値を出力
+			System.out.println("絶対値は" + integerValue + "です。");
+		// 整数値が0より小さい場合
+		else if (integerValue < 0)
+			// 絶対値の値を求めるためマイナスを反転させて出力
+			System.out.println("絶対値は" + -integerValue + "です。");
+		// 0である場合（それ以外）
+		else
+			// 絶対値の値を出力
+			System.out.println("絶対値は" + integerValue + "です。");
+
+	}
+}
