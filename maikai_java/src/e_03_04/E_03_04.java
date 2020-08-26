6c8387e5c7a5bbf016870118e54961198fa51755
hoshikawa megumi <ddt.mehoshikawa@gmail.com>
2020-08-26 20:58:16 +0900

    　E03_04 　ご指摘修正(仕様通りの実装に修正) 　100％

--- maikai_java/src/e_03_04/E_03_04.java
+++ maikai_java/src/e_03_04/E_03_04.java
@@ -15,33 +15,29 @@ public class E_03_04 {
 
 	public static void main(String[] args) {
 
-		/*
-		 * a社会の点数を、 b理科の点数と置き換え、プログラムを作成する
-		 */
-
-		Scanner testScore = new Scanner(System.in);
+		Scanner scanVariable = new Scanner(System.in);
 
 		// 読み込まれた値を抽出。
-		System.out.print("社会の点数 ：");
+		System.out.print("変数a ：");
 		// 抽出された値を代入。
-		int socialScore = testScore.nextInt();
+		int firstVariable = scanVariable.nextInt();
 		// 読み込まれた値を抽出。
-		System.out.print("理科の点数 ： ");
+		System.out.print("変数b ： ");
 		// 抽出された値を代入。
-		int scienceScore = testScore.nextInt();
+		int nextVariable = scanVariable.nextInt();
 
-		// 社会の点数が大きい場合
-		if (socialScore > scienceScore) {
+		// 変数aが大きい場合
+		if (firstVariable > nextVariable) {
 			// どちらが大きいか結果を出力
-			System.out.println("社会の点数のほうが高いです。");
-			// 理科の点数が大きい場合
-		} else if (socialScore < scienceScore) {
+			System.out.println("aのほうが大きいです。");
+			// 変数bが大きい場合
+		} else if (firstVariable < nextVariable) {
 			// どちらが大きいか結果を出力
-			System.out.println("理科の点数のほうが高いです。");
-			// 社会の点数と理科の点数が同じの場合
-		} else if (socialScore == scienceScore) {
+			System.out.println("bのほうが大きいです。");
+			// 変数aと変数bが同じの場合
+		} else if (firstVariable == nextVariable) {
 			// どちらが大きいか結果を出力
-			System.out.println("社会の点数・理科の点数どちらも同じ点数です。");
+			System.out.println("aとbは同じ値です。");
 		}
 	}
 }
