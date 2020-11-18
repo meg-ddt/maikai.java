753cf5f2b78d4aedd3b883552187b79c85b1ba48
hoshikawa megumi <ddt.mehoshikawa@gmail.com>
2020-11-18 20:14:35 +0900

    　E03_09 　ご指摘修正(実数値の比較・項目名の修正) 　100％

--- maikai_java/src/e_03_09/E_03_09.java
+++ maikai_java/src/e_03_09/E_03_09.java
@@ -15,16 +15,16 @@ public class E_03_09 {
 		// 文言の表示
 		System.out.print("実数値を入力してください：実数値1＝");
 		// 入力された実数値を代入
-		int actualValue1 = integerValue.nextInt();
+		double actualValue1 = integerValue.nextDouble();
 		// 文言の表示
 		System.out.print("実数値を入力してください：実数値2＝");
 		// 入力された実数値を代入
-		int actualValue2 = integerValue.nextInt();
+		double actualValue2 = integerValue.nextDouble();
 
-		// 実数値1と2を比較
-		int actualValue = actualValue1 > actualValue2 ? actualValue1 : actualValue2;
+		// 実数値1が実数値2より大きい場合、実数値1を結果に代入し、そうでない場合は実数値2を結果に代入する。
+		double largeValue = actualValue1 > actualValue2 ? actualValue1 : actualValue2;
 		// 比較した結果を表示
-		System.out.println("2つの実数値を比べた結果、" + actualValue + "の値が大きいです。");
+		System.out.println("2つの実数値を比べた結果、" + largeValue + "の値が大きいです。");
 
 	}
 }
